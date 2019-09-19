98c97bb4217da9e1e64665ea47c0e3092fd23533
Aidan Rosen <aidanrosen04@gmail.com>
2019-09-19 14:19:58 -0700

    Initial Commit

---
+++ GitMidTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GitMidTest
+//
+//  Created by Rosen, Aidan on 9/19/19.
+//  Copyright © 2019 Rosen, Aidan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
